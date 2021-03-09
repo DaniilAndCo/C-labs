d605a53f70fcba9975ab359e0581a79e1460d76e
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-10 01:10:10 +0300

    fixed tests

--- first_lab/tests.c
+++ first_lab/tests.c
@@ -2,18 +2,18 @@
 #include <assert.h>
 
 void testCreateBinaryNumber(){
-    assert(CreateBinaryNumber(0, "") == "111");
-    assert(CreateBinaryNumber(1, "111") == "111000");
-    assert(CreateBinaryNumber(2, "111000") == "111000111");
-    assert(CreateBinaryNumber(3, "111000111") == "111000111000");
+    assert(!strcmp(CreateBinaryNumber(0, ""), "111"));
+    assert(!strcmp(CreateBinaryNumber(1, "111"), "111000"));
+    assert(!strcmp(CreateBinaryNumber(2, "111000"), "111000111"));
+    assert(!strcmp(CreateBinaryNumber(3, "111000111"), "111000111000"));
     return;
 }
 
 void testConcat(){
-    assert(Concat("1", "2") == "12");
-    assert(Concat("", "2") == "2");
-    assert(Concat("3-5", "=5") == "3-5=5");
-    assert(Concat("25", "*2=2") == "25*2=2");
+    assert(!strcmp(Concat("1", "2"), "12"));
+    assert(!strcmp(Concat("", "2"), "2"));
+    assert(!strcmp(Concat("3-5", "=5"), "3-5=5"));
+    assert(!strcmp(Concat("25", "*2=2"), "25*2=2"));
     return;
 }
 #undef main
