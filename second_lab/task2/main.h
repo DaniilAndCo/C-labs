5c0fb9ec870d1eccb0c6f7768e88c4c756360380
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-10 10:43:58 +0300

    Delete second_lab/task2 directory

--- second_lab/task2/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#define true 1
-#define false 0
-typedef unsigned short bool;
-double Factorial(int num);
-double* MySin(const double* x, const double* inf);
-bool IsCorrect(char* strNumber);
-#endif
