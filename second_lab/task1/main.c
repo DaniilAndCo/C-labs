68d1c0537c2eabe1800d8e2be375fcf8c2e508b9
Daniil Bondarcov <1234567890bonddan@mail.ru>
2021-03-10 11:36:30 +0300

    linter

--- second_lab/task1/main.c
+++ second_lab/task1/main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <math.h>
+
 #define true 1
 #define false 0
 typedef unsigned short bool;
-#define ARGUMENTS_0 &firstNumber_0, &firstNumber_1, &secondNumber_0, &secondNumber_1
-#define ARGUMENTS_1 int* firstNumber_0, int* firstNumber_1, int* secondNumber_0, int* secondNumber_1
+#define ARGUMENTS_0 & firstNumber_0, & firstNumber_1, & secondNumber_0, & secondNumber_1
+#define ARGUMENTS_1 int * firstNumber_0, int * firstNumber_1, int * secondNumber_0, int * secondNumber_1
 
-double Sum(const double first, const double second)
-{
+double Sum(const double first,
+    const double second) {
     return first + second;
 }
-double Substract(const double first, const double second)
-{
+double Substract(const double first,
+    const double second) {
     return first - second;
 }
-double Division(const double first, const double second)
-{
+double Division(const double first,
+    const double second) {
     return first / second;
 }
-double Multiplying(const double first, const double second)
-{
+double Multiplying(const double first,
+    const double second) {
     return first * second;
 }
-double AverageGarmonic(const double first, const double second)
-{
+double AverageGarmonic(const double first,
+    const double second) {
     return 2 / (1 / first + 1 / second);
 }
 /* Clear buffer trash*/
@@ -36,26 +40,21 @@ void MyFlush(void) {
     } while (c != EOF && c != '\n');
     return;
 }
-void CorrectInputINT(int* inputVar)
-{
-    while (!scanf("%d", inputVar))
-    {
+void CorrectInputINT(int * inputVar) {
+    while (!scanf("%d", inputVar)) {
         MyFlush();
         printf("Enter valid data!\n");
     }
     return;
 }
-void CorrectInputLF(double* inputVar)
-{
-    while (!scanf("%lf", inputVar))
-    {
+void CorrectInputLF(double * inputVar) {
+    while (!scanf("%lf", inputVar)) {
         MyFlush();
         printf("Enter valid data!\n");
     }
     return;
 }
-void PrintMenu()
-{
+void PrintMenu() {
     printf("1. Enter two rational numbers\n");
     printf("2. Show two rational numbers\n");
     printf("3. Sum of two rational numbers\n");
@@ -67,77 +66,64 @@ void PrintMenu()
     printf("9. Exit\n");
     return;
 }
-void EnterValues(ARGUMENTS_1)
-{
+void EnterValues(ARGUMENTS_1) {
     printf("numerator of first one: ");
     CorrectInputINT(firstNumber_0);
     printf("denominator of first one(!=0): ");
-    do
-    {
-    CorrectInputINT(firstNumber_1);
-    } while (*firstNumber_1 == 0);
+    do {
+        CorrectInputINT(firstNumber_1);
+    } while ( * firstNumber_1 == 0);
     printf("numerator of second one: ");
     CorrectInputINT(secondNumber_0);
     printf("denominator of second one(!=0): ");
-    do
-    {
+    do {
         CorrectInputINT(secondNumber_1);
-    } while (*secondNumber_1 == 0);
+    } while ( * secondNumber_1 == 0);
     return;
 }
-void ShowValues(ARGUMENTS_1)
-{
+void ShowValues(ARGUMENTS_1) {
     printf("Two rational numbers: ");
-    printf("%d / %d and %d / %d\n", *firstNumber_0, *firstNumber_1, *secondNumber_0, *secondNumber_1);
+    printf("%d / %d and %d / %d\n", * firstNumber_0, * firstNumber_1, * secondNumber_0, * secondNumber_1);
     return;
 }
-void ShowSum(ARGUMENTS_1)
-{
-    printf("%d / %d + %d / %d = %f\n", *firstNumber_0, *firstNumber_1, *secondNumber_0, *secondNumber_1,
-        Sum((*firstNumber_0 + 0.0) / *firstNumber_1, (*secondNumber_0 + 0.0) / *secondNumber_1));
+void ShowSum(ARGUMENTS_1) {
+    printf("%d / %d + %d / %d = %f\n", * firstNumber_0, * firstNumber_1, * secondNumber_0, * secondNumber_1,
+        Sum(( * firstNumber_0 + 0.0) / * firstNumber_1, ( * secondNumber_0 + 0.0) / * secondNumber_1));
     return;
 }
-void ShowSubstract(ARGUMENTS_1)
-{
-    printf("%d / %d - %d / %d = %f\n", *firstNumber_0, *firstNumber_1, *secondNumber_0, *secondNumber_1,
-        Substract((*firstNumber_0 + 0.0) / *firstNumber_1, (*secondNumber_0 + 0.0) / *secondNumber_1));
+void ShowSubstract(ARGUMENTS_1) {
+    printf("%d / %d - %d / %d = %f\n", * firstNumber_0, * firstNumber_1, * secondNumber_0, * secondNumber_1,
+        Substract(( * firstNumber_0 + 0.0) / * firstNumber_1, ( * secondNumber_0 + 0.0) / * secondNumber_1));
     return;
 }
-void ShowDivision(ARGUMENTS_1)
-{
-    printf("%d / %d : %d / %d = %f\n", *firstNumber_0, *firstNumber_1, *secondNumber_0, *secondNumber_1,
-        Division((*firstNumber_0 + 0.0) / *firstNumber_1, (*secondNumber_0 + 0.0) / *secondNumber_1));
+void ShowDivision(ARGUMENTS_1) {
+    printf("%d / %d : %d / %d = %f\n", * firstNumber_0, * firstNumber_1, * secondNumber_0, * secondNumber_1,
+        Division(( * firstNumber_0 + 0.0) / * firstNumber_1, ( * secondNumber_0 + 0.0) / * secondNumber_1));
     return;
 }
-void ShowMultiplying(ARGUMENTS_1)
-{
-    printf("%d / %d x %d / %d = %f\n", *firstNumber_0, *firstNumber_1, *secondNumber_0, *secondNumber_1,
-        Multiplying((*firstNumber_0 + 0.0) / *firstNumber_1, (*secondNumber_0 + 0.0) / *secondNumber_1));
+void ShowMultiplying(ARGUMENTS_1) {
+    printf("%d / %d x %d / %d = %f\n", * firstNumber_0, * firstNumber_1, * secondNumber_0, * secondNumber_1,
+        Multiplying(( * firstNumber_0 + 0.0) / * firstNumber_1, ( * secondNumber_0 + 0.0) / * secondNumber_1));
     return;
 }
-void ShowAverageGarmonic(ARGUMENTS_1)
-{
-    printf("average garmonic = %f\n", AverageGarmonic((*firstNumber_0 + 0.0) / *firstNumber_1, (*secondNumber_0 + 0.0) / *secondNumber_1));
+void ShowAverageGarmonic(ARGUMENTS_1) {
+    printf("average garmonic = %f\n", AverageGarmonic(( * firstNumber_0 + 0.0) / * firstNumber_1, ( * secondNumber_0 + 0.0) / * secondNumber_1));
     return;
 }
-void ShowInfo()
-{
+void ShowInfo() {
     printf("The author is Daniel Bondarcov 053501\nversion of the program - 1.0.0\n");
     return;
 }
 
-int main()
-{
+int main() {
     PrintMenu();
     int manageVar = 0;
     int firstNumber_0 = 0, firstNumber_1 = 0;
     int secondNumber_0 = 0, secondNumber_1 = 0;
     bool flag = true;
-    while (manageVar != 9 && flag)
-    {
-        CorrectInputINT(&manageVar);
-        switch (manageVar)
-        {
+    while (manageVar != 9 && flag) {
+        CorrectInputINT( & manageVar);
+        switch (manageVar) {
         case 1:
             EnterValues(ARGUMENTS_0);
             break;
