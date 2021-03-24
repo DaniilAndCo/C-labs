41b08031706c2d08b4d9423b02d0e4553b849845
Daniil <1234567890bonddan@mail.ru>
2021-03-24 11:24:29 +0300

    add fabs to tests

--- third_lab/tests.c
+++ third_lab/tests.c
@@ -4,19 +4,19 @@
 void testWaterFlow(){
     unsigned int s = 1, h = 1, v = 1;
     double inf = 0.01;
-    assert(CalculateWaterFlow(&h, &s, &v) - 1.5708 < inf);
+    assert(fabs(CalculateWaterFlow(&h, &s, &v) - 1.5708) < inf);
     h = 2;
     s = 1;
     v = 36;
-    assert(CalculateWaterFlow(&h, &s, &v) - 113.097 < inf);
+    assert(fabs(CalculateWaterFlow(&h, &s, &v) - 113.097) < inf);
     h = 4;
     s = 20;
     v = 6;
-    assert(CalculateWaterFlow(&h, &s, &v) - 753.982 < inf);
+    assert(fabs(CalculateWaterFlow(&h, &s, &v) - 753.982) < inf);
     h = 6;
     s = 150;
     v = 2;
-    assert(CalculateWaterFlow(&h, &s, &v) - 2827.433 < inf);
+    assert(fabs(CalculateWaterFlow(&h, &s, &v) - 2827.433) < inf);
     return;
 }
 
